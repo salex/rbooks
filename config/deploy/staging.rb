c3ff885b0f8d4a2bf538fe8f81ed5bf489a57d43
Steve Alex <salex@mac.com>
2020-11-08 13:26:43 -0600

    out of sync

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -82,8 +82,8 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/main`
-        puts "WARNING: HEAD is not the same as origin/main"
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
         puts "Run `git push` to sync changes."
         exit
       end
