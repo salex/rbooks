c25075a510657b19f6e829c77fed06d9889cd4d1
Steve Alex <salex@mac.com>
2021-11-28 04:53:51 -0600

    bad tailwind install???

--- babel.config.js
+++ babel.config.js
@@ -71,7 +71,9 @@ module.exports = function(api) {
         {
           async: false
         }
-      ]
+      ],
+      ['@babel/plugin-proposal-private-methods', { loose: true }],
+      ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
     ].filter(Boolean)
   }
 }
