7124ba40f38a73cdef5fcd0d16ce3951ee5e8fa5
Steve Alex <salex@mac.com>
2021-11-28 04:58:23 -0600

    bable crap

--- babel.config.js
+++ babel.config.js
@@ -71,9 +71,7 @@ module.exports = function(api) {
         {
           async: false
         }
-      ],
-      ['@babel/plugin-proposal-private-methods', { loose: true }],
-      ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
+      ]
     ].filter(Boolean)
   }
 }
